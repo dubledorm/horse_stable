8ab785bf23457c2ef5ea7ccd2dfd8e63dc41cab0
ubuntu <dubledorm@gmail.com>
2022-10-12 16:09:49 -0400

    Исаправлен файл production.rb с целью использования переменных окружения.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,12 +32,12 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV['LOCALHOST_URL'], port: ENV['LOCALHOST_PORT'] }
   config.action_mailer.smtp_settings = {
       user_name:      ENV['SENDMAIL_USERNAME'],
       password:       ENV['SENDMAIL_PASSWORD'],
